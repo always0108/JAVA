9a0eb952b09b35e8fee874cccb8722d526a30f24
limeng <ldx19980108@gmail.com>
2018-08-18 23:59:42 +0800

    fix some bugs

--- WebCrawler/NewJWGL/src/main/java/Main.java
+++ WebCrawler/NewJWGL/src/main/java/Main.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-//        Scanner input = new Scanner(System.in);
-//        System.out.print("请输入学号:");
-//        String stuNum = input.next();
-//        System.out.print("请输入密码:");
-//        String password = input.next();
-
-        ConnectJWGL connectJWGL = new ConnectJWGL("04163164","ldxz19980108!@");
+        Scanner input = new Scanner(System.in);
+        System.out.print("请输入学号:");
+        String stuNum = input.next();
+        System.out.print("请输入密码:");
+        String password = input.next();
+        ConnectJWGL connectJWGL = new ConnectJWGL(stuNum,password);
         connectJWGL.init();
 
     }
